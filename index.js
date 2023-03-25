4ed9a72f21d53e23aee2270872d8cfebc413dc64
Jhon Michael Babadilla <125885842+JMBabadilla@users.noreply.github.com>
2023-03-25 13:58:54 +0000

    commit

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-function play(playerMove) {
-	var moves = ['rock', 'paper', 'scissors'];
-	var computerMove = moves[Math.floor(Math.random() * moves.length)];
-	
-	var result;
-	if (playerMove === computerMove) {
-		result = 'Tie!';
-	} else if (
-		(playerMove === 'rock' && computerMove === 'scissors') ||
-		(playerMove === 'paper' && computerMove === 'rock') ||
-		(playerMove === 'scissors' && computerMove === 'paper')
-	) {
-		result = 'You win!';
-	} else {
-		result = 'Computer wins!';
-	}
-	
-	document.getElementById('result').innerHTML = `You played ${playerMove}, computer played ${computerMove}. ${result}`;
+let playerWins = 0;
+let computerWins = 0;
+let ties = 0;
+
+function play(playerChoice) {
+  const choices = ["rock", "paper", "scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  document.getElementById("computer-choice").textContent = computerChoice;
+
+  let result;
+  if (playerChoice === computerChoice) {
+    result = "Tie";
+    ties++;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = "You win";
+    playerWins++;
+  } else {
+    result = "Computer wins";
+    computerWins++;
+  }
+
+  const historyItem = `${playerChoice} vs. ${computerChoice}: ${result}`;
+  const historyList = document.getElementById("history");
+  const historyItemElement = document.createElement("div");
+  historyItemElement.textContent = historyItem;
+  historyList.prepend(historyItemElement);
+
+}
+
+function del_btn(){
+    let menu_list = document.querySelector('#history');
+    menu_list.removeChild(menu_list.lastElementChild);
 }
